89f979ed0ab2c6471b093aa9fa356db7b6fcf7e8
Hopeakondeng <hopeakondeng@gmail.com>
2022-05-09 16:06:49 +0100

    new files

---
+++ week 2/Day 1/index.js
@@ -0,0 +1,21 @@
+//let a = 'chocolate'
+//let b = 'dinner'
+//console.log('I eat '+ a + ' at every '+ b)
+
+//let c = ["black mirror", "money heist", "the big bang theory"];
+//let d = c[0]+ ',' + c[1]+ ' and '+ c[2]+ '.';
+//let e = c.length
+//console.log('I watched '+ e+ ' series : '+d)
+//c[2] = 'Friends'
+//c.push('The Witcher')
+//c.unshift('Euphoria')
+//c.splice(1,1)
+//console.log(c[1][2])
+
+//console.log(c)
+
+
+let temp = 37
+let f = temp/5 *9 + 32
+console.log(temp+'°C is '+ f+ '°F')
+
